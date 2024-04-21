7e2cabbd266baab17d4c66fff352b4d0b48630c0
monki1 <114178697+monki1@users.noreply.github.com>
2024-04-21 02:55:06 -0400

    new context collection

--- context.sh
+++ context.sh
@@ -1,3 +1,15 @@
-rm all_files_content.txt
-find . -type d \( -path './bin' -o -path './.dart_tool' -o -path './.git' \) -prune -o \( -name '.gitignore' -prune \) -o -type f -print | xargs -I{} sh -c 'echo "File Path: {}" >> all_files_content.txt; echo "Content:" >> all_files_content.txt; cat "{}" >> all_files_content.txt; echo "" >> all_files_content.txt;'
-pbcopy < all_files_content.txt
+# Remove context.txt if it exists
+[ -f context.txt ] && rm context.txt
+
+# Initialize context.txt with a header
+echo "Files and Content:" > context.txt
+
+# Append all file paths excluding specified files and directories to context.txt
+find . -type d \( -path './bin' -o -path './.dart_tool' -o -path './.git' \) -prune -o -type f ! \( -name '.gitignore' -o -name 'context.txt' -o -name 'pubspec.lock' -o -name 'context.sh' -o -name 'analysis_options.yaml' \) -print | tee -a context.txt | while read -r file; do
+    echo "Content for $file:" >> context.txt
+    cat "$file" >> context.txt
+    echo "" >> context.txt
+done
+
+# Copy the contents of context.txt to the clipboard
+pbcopy < context.txt
